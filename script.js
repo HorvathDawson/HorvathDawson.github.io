27260eaca85ffea7a860c2523e3fe037403390f5
Dawson Horvath <horvath.dawson@gmail.com>
2024-11-30 00:34:42 -0800

    wip

--- script.js
+++ script.js
@@ -8,4 +8,30 @@ document.addEventListener("DOMContentLoaded", () => {
             header.classList.remove("scrolled");
         }
     });
+
+    // window.addEventListener("scroll", () => {
+    //     const hero = document.querySelector(".hero");
+    //     const background = document.querySelector(".background");
+    //     const textContainer = document.querySelector(".text-container");
+    
+    //     const bgRect = background.getBoundingClientRect();
+    //     const textRect = textContainer.getBoundingClientRect();
+
+    //     const scrollY = window.scrollY;
+    //     background.style.top = `-${scrollY}px`;
+
+    //     console.log(bgRect.bottom, textRect.bottom)
+    
+    //     if (bgRect.bottom <= 50 && textRect.bottom >= 0) {
+    //         // Stick background when it reaches 50px from the top
+    //         console.log(background.offsetHeight)
+    //         const offset = background.offsetHeight - 150
+    //         background.style.top = `-${offset}px`;
+    //         console.log("Stick background")
+
+    //     } 
+    // });
+    
+    
 });
+
