9dc7f0c002232baea044738baa96c0310380f601
Dawson Horvath <horvath.dawson@gmail.com>
2024-12-08 12:00:23 -0800

    only css parallax

---
+++ original/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.getElementById("main-header");
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 0) {
+            header.classList.add("scrolled");
+        } else {
+            header.classList.remove("scrolled");
+        }
+
+        // const heroScrollOver = document.querySelector(".hero .scroll-over");
+        // if (heroScrollOver.getBoundingClientRect().top < header.offsetHeight) {
+        //     header.classList.add("white-section");
+        // } else {
+        //     header.classList.remove("white-section");
+        // }
+    });
+
+
+
+    
+});
+
--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const header = document.getElementById("main-header");
-
-    window.addEventListener("scroll", () => {
-        if (window.scrollY > 0) {
-            header.classList.add("scrolled");
-        } else {
-            header.classList.remove("scrolled");
-        }
-
-        const heroScrollOver = document.querySelector(".hero .scroll-over");
-        if (heroScrollOver.getBoundingClientRect().top < header.offsetHeight) {
-            header.classList.add("white-section");
-        } else {
-            header.classList.remove("white-section");
-        }
-    });
+// JavaScript for Parallax Effect
+document.addEventListener('DOMContentLoaded', () => {
+ 
+ 
+    document.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY; // Get current scroll position
+    console.log(scrollTop);
+    // const layers = document.querySelectorAll('.parallax__layer');
+    // layers.forEach((layer, index) => {
+    //   console.log(layer, index);
+    //   const speed = (index + 1) * 0.3; // Define layer-specific speed
+    //   const yOffset = scrollTop * speed;
+    //   layer.style.transform = `translateY(${yOffset}px)`;
+    // });
+  });
 
 
-
-    
 });
-
+  
