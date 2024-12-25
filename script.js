828445e2dffc2cefd4567303f51ea79eaf323f01
Dawson Horvath <horvath.dawson@gmail.com>
2024-12-24 16:10:51 -0800

    darcy basics

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const header = document.getElementById("main-header");
 
+    const headers = document.getElementsByClassName("main-header");
     window.addEventListener("scroll", () => {
-        if (window.scrollY > 0) {
-            header.classList.add("scrolled");
-        } else {
-            header.classList.remove("scrolled");
-        }
+        Array.from(headers).forEach(header => {
+            if (window.scrollY > 0) {
+                header.classList.add("scrolled");
+            } else {
+                header.classList.remove("scrolled");
+            }
+        });
     });
 
     // window.addEventListener("scroll", () => {
